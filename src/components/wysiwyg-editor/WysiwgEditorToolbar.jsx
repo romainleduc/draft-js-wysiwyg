2f3dad37ca47d163aae92c69106ca1e76a34a312
KiziLom <romain.andre.leduc.dev@gmail.com>
2020-09-08 17:23:46 +0200

    style

--- src/components/wysiwyg-editor/WysiwgEditorToolbar.jsx
+++ src/components/wysiwyg-editor/WysiwgEditorToolbar.jsx
@@ -3,20 +3,23 @@ import React from 'react';
 const WysiwygEditorToolbar = ({
     itemsProps,
     ...props
-}) => {
-    return (
-        <div className='toolbar'>
-            {itemsProps &&
-                itemsProps.map((item, itemKey) =>
-                    <div key={`items-wrapper-${itemKey}`} className={item.className}>
-                        {item.components.map((component, componentKey) =>
-                            React.cloneElement(component, { key: `component-${componentKey}`, ...props })
-                        )}
-                    </div>
-                )}
-        </div>
-    );
-}
+}) => (
+    <div className='toolbar'>
+        {itemsProps &&
+            itemsProps.map((item, itemKey) =>
+                <div key={`items-wrapper-${itemKey}`} className={item.className}>
+                    {item.components.map((component, componentKey) =>
+                        React.cloneElement(
+                            component,
+                            {
+                                key: `component-${componentKey}`,
+                                ...props,
+                            }
+                        )
+                    )}
+                </div>
+            )}
+    </div>
+);
 
-WysiwygEditorToolbar.propTypes = propTypes;
 export default WysiwygEditorToolbar;
