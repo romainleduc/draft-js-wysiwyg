77a29c62770759867584b70aaa61bc8c1b4209aa
KiziLom <romain.leduc.dev@gmail.com>
2020-09-28 20:20:31 +0200

    Remove prefab toggle button

--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ export { EditorToolbar } from './EditorToolbar';
 export { InlineToggleButton } from './InlineToggleButton';
 export { BlockTypeToggleButton } from './BlockTypeToggleButton';
 export { TextAlignToggleButton } from './TextAlignToggleButton';
-export { PrefabToggleButton } from './PrefabToggleButton';
 export { EditorDraft } from './EditorDraft';
