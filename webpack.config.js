9f47a1180e268126363faa3c7b7eac5cd96f1d80
KiziLom <romain.leduc.dev@gmail.com>
2020-09-29 23:17:10 +0200

    Remove ts-loader devDependencie

--- webpack.config.js
+++ webpack.config.js
@@ -21,15 +21,6 @@ module.exports = {
             },
         ]
     },
-    // module: {
-    //     rules: [
-    //         {
-    //             test: /\.(js|jsx)$/,
-    //             exclude: /node_modules/,
-    //             use: 'babel-loader',
-    //         },
-    //     ]
-    // },
     resolve: {
         extensions: ['.ts', '.tsx', '.js'],
     },
