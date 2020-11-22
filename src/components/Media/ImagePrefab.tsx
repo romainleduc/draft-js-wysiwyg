35da6e8b77f879dbc7dbe27a85acfbc9ff4ab3f8
KiziLom <romain.leduc.dev@gmail.com>
2020-11-22 16:26:43 +0100

    preti

--- src/components/Media/ImagePrefab.tsx
+++ src/components/Media/ImagePrefab.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-interface ImagePrefabProps extends Omit<React.ImgHTMLAttributes<HTMLImageElement>, 'src'> {
+interface ImagePrefabProps
+  extends Omit<React.ImgHTMLAttributes<HTMLImageElement>, 'src'> {
   src: string | string[];
   sourceProps?: React.SourceHTMLAttributes<HTMLSourceElement>[];
 }
@@ -9,23 +10,21 @@ export const ImagePrefab = ({
   src,
   sourceProps,
   ...other
-}: ImagePrefabProps): JSX.Element => {
-  return (
-    <>
-      {!Array.isArray(src) ? (
-        <img src={src} {...other} />
-      ) : (
-        <picture>
-          {src.map((srcImage, key) => {
-            if (key !== 0) {
-              const props = sourceProps?.[key];
+}: ImagePrefabProps): JSX.Element => (
+  <>
+    {!Array.isArray(src) ? (
+      <img src={src} {...other} />
+    ) : (
+      <picture>
+        {src.map((srcImage, key) => {
+          if (key !== 0) {
+            const props = sourceProps?.[key];
 
-              return <source src={srcImage} {...props} />;
-            }
-          })}
-          <img src={src[0]} {...other} />
-        </picture>
-      )}
-    </>
-  );
-};
+            return <source src={srcImage} {...props} />;
+          }
+        })}
+        <img src={src[0]} {...other} />
+      </picture>
+    )}
+  </>
+);
